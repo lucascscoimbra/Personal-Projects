f156bc02e82f522aea7a72509ece21b15b1d2abf
Lucas Coimbra <lucascoimbra@protonmail.com>
2021-04-28 17:03:26 -0300

    String text was changed
    String Text was altered from "First Test" to "Second Test".

--- Append-text.java
+++ Append-text.java
@@ -8,7 +8,7 @@ public class AppendFile {
     public static void main(String[] args) {
 
         String path = System.getProperty("user.dir") + "\\src\\test.txt";
-        String text = "First Test";
+        String text = "Second Test";
 
         try {
             Files.write(Paths.get(path), text.getBytes(), StandardOpenOption.APPEND);
